712796cace8e009b8950276070119b1ccd0e43dc
bharath4IT <118511946+bharath4IT@users.noreply.github.com>
2022-11-18 16:06:22 +0530

    Different Data Types in Java
    Data Types               Default Size
    1.boolean data type- 1 bit
    2.byte data type- 2 byte
    3.char data type- 1 byte
    4.short data type- 2 byte
    5.int data type- 4 byte
    6.long data type- 8 byte
    7.float data type- 4 byte
    8.double data type- 8 byte

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    // Boolean Data Type
+    Boolean one = false;
+
+    // Byte Data Type
+    byte a = 10;
+    byte b = -20;
+
+    // Short Data Type
+    short s = 10000;
+    short r = -5000;
+
+    // Int Data Type
+    int x = 100000;
+    int y = -200000;
+
+    // Long Data Type
+    long p = 100000L;
+    long q = -200000L;
+
+    // Double Data Type
+    double d1 = 12.3;
+
+    // Char Data Type
+    char letterA = 'A';
+
+}
